43ea444e1a2538487239ac47d414739b4e021b4c
htbarbosa <henrique_telesbarbosa@hotmail.com>
2021-10-01 17:22:51 -0300

    add gitignore

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const app = express();
 
 const PORT = 3000;
 
-// //the guardian exemple
-// const url = "https://www.theguardian.com/uk";
+//the guardian exemple
+const url = "https://www.theguardian.com/uk";
+axios(url)
+    .then(res => {
+        const html = res.data;
+        const $ = cheerio.load(html);
+        const dataAccumulator = [];
+
+        // the selectors to scrap the page are below and subject to change acording to the page being scraped
+        $(".fc-item__title", html).each(function () {
+            // dom element exemple to be scraped 
+            const title = $(this).text();
+            const link = $(this).find('a').attr('href');
+            dataAccumulator.push({
+                title,
+                link
+            });
+        });
+
+        console.log(dataAccumulator);
+    }).catch(err => console.log(err));
+ 
+
+
+// const url = "https://exemple.com/";
+
 // axios(url)
 //     .then(res => {
 //         const html = res.data;
@@ -14,44 +38,19 @@ const PORT = 3000;
 //         const dataAccumulator = [];
 
 //         // the selectors to scrap the page are below and subject to change acording to the page being scraped
-//         $(".fc-item__title", html).each(function () {
+//         $(".css_class", html).each(function () {
 //             // dom element exemple to be scraped 
 //             const title = $(this).text();
-//             const link = $(this).find('a').attr('href');
+//             const link = $(this).attr('href');
 //             dataAccumulator.push({
 //                 title,
 //                 link
 //             });
 //         });
-
 //         console.log(dataAccumulator);
+//         return res.json(dataAccumulator);
 //     }).catch(err => console.log(err));
-
-
-
-app.get("/", (req, res) => {
-    const url = "https://sensoincomum.org/";
-
-    axios(url)
-        .then(res => {
-            const html = res.data;
-            const $ = cheerio.load(html);
-            const dataAccumulator = [];
-
-            // the selectors to scrap the page are below and subject to change acording to the page being scraped
-            $(".vc_gitem-link", html).each(function () {
-                const title = $(this).text();
-                const link = $(this).attr('href');
-                dataAccumulator.push({
-                    title,
-                    link
-                });
-            });
-            console.log(dataAccumulator);
-            return res.json(dataAccumulator);
-        }).catch(err => console.log(err));
-    return res.json(dataAccumulator);
-});
+// console.log(dataAccumulator);
 
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
